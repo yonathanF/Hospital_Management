35e9da5ffb6a71cfa52dae7e4f2ca2e6181d969f
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-04-21 01:29:48 -0400

    almost done with patient views

--- HospitalManagement/Patient/urls.py
+++ HospitalManagement/Patient/urls.py
@@ -2,4 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [url(r'^/home', views.home, name='home')]
+urlpatterns = [
+    url(r'^/home', views.home, name='home'),
+    url(r'^/appointments', views.appointments, name='appointments'),
+    url(r'^/new_appointments', views.new_appointments,
+        name='new_appointments'),
+    url(r'^/medications', views.medications, name='medications'),
+    url(r'^/bills', views.bills, name='bills')
+]
--- HospitalManagement/Patient/views.py
+++ HospitalManagement/Patient/views.py
@@ -6,3 +6,39 @@ from django.shortcuts import redirect, render
 def home(request):
     ''' a patient private home page '''
     return HttpResponse(render(request, 'Patient/home.html'))
+
+
+def appointments(request):
+    ''' handles the creation and display of appts '''
+
+    return HttpResponse(render(request, 'Patient/appt.html'))
+
+
+def new_appointments(request):
+    ''' handles the creation and display of appts '''
+
+    if request.method == 'POST':
+        # get the data
+        doctor = request.POST.get("preferedDoctor", "")
+        apt_date = request.POST.get("preferedDate", "")
+        reason = request.POST.get("reason", "")
+
+        #TODO insert into db
+        return redirect("/patient/home/")
+
+    else:
+        return HttpResponse(render(request, 'Patient/new_appt.html'))
+
+
+def medications(request):
+    ''' mediations info page'''
+
+    # no post, handle all as get
+    return HttpResponse(render(request, 'Patient/medications.html'))
+
+
+def bills(request):
+    ''' shows the user profile '''
+
+    # also no posts, all get
+    return HttpResponse(render(request, 'Patient/bills.html'))
