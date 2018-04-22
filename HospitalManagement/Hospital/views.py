d7043f4b5cb1de3bd116f2f50acc334b79f8f1aa
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-04-22 00:49:10 -0400

    home page for admin done

--- HospitalManagement/Hospital/views.py
+++ HospitalManagement/Hospital/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def home(request):
+    ''' the home for hosptial admins '''
+
+    # handle get only
+    return HttpResponse(render(request, 'Hosptial/home.html'))
