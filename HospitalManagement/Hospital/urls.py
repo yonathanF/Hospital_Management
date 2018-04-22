ed90220bb008f3d6b2655474b1373f0c291948d3
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-04-22 13:49:03 -0400

    minor changes for new url routing etc

--- HospitalManagement/Hospital/urls.py
+++ HospitalManagement/Hospital/urls.py
@@ -4,5 +4,17 @@ from . import views
 
 urlpatterns = [
     url(r'^/home', views.home, name='home'),
-    url(r'^/search', views.search, name='search')
+    url(r'^/search', views.search, name='search'),
+    url(r'^/appointments', views.appointments, name='appointments'),
+    url(r'^/patients', views.patients, name='patients'),
+    url(r'^/doctors', views.doctors, name='doctors'),
+    url(r'^/rooms', views.rooms, name='rooms'),
+    url(r'^/departments', views.departments, name='departments'),
+    url(r'^/bills', views.bills, name='bills'),
+    url(r'^/delete_bill/(?P<bill_num>[0-9]+)/$',
+        views.delete_bill,
+        name='delete_bill'),
+    url(r'^/update_appointment/(?P<patient_id>[0-9]+)/(?P<doc_id>[0-9]+/)$',
+        views.update_appointment,
+        name='update_appointment')
 ]
--- HospitalManagement/Hospital/views.py
+++ HospitalManagement/Hospital/views.py
@@ -14,3 +14,51 @@ def search(request):
     ''' search for firstname, lastname, email '''
 
     return HttpResponse(render(request, 'Hosptial/search.html'))
+
+
+def appointments(request):
+    ''' handles appointments '''
+
+    return HttpResponse(render(request, 'Hosptial/appointments.html'))
+
+
+def update_appointment(request, patient_id, doc_id):
+    ''' handles appointments update '''
+
+    return HttpResponse(render(request, 'Hosptial/update_apt.html'))
+
+
+def patients(request):
+    ''' shows all patients and leads to profile '''
+
+    return HttpResponse(render(request, 'Hosptial/patients.html'))
+
+
+def doctors(request):
+    ''' shows all doctors and their info '''
+
+    return HttpResponse(render(request, 'Hosptial/doctors.html'))
+
+
+def rooms(request):
+    ''' shows info about rooms: handles get only '''
+
+    return HttpResponse(render(request, 'Hosptial/rooms.html'))
+
+
+def departments(request):
+    ''' shows dep info: handles get only '''
+
+    return HttpResponse(render(request, 'Hosptial/departments.html'))
+
+
+def bills(request):
+    ''' handles the view and creation of bills '''
+
+    return HttpResponse(render(request, 'Hosptial/bills.html'))
+
+
+def delete_bill(request, bill_num):
+    ''' deletes a bill given the bill_id '''
+
+    return redirect('/hosptial/bills')
