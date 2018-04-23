248774d0914e1124c41928bc4621572551715488
Sonwoo Kim <sak2km@gmail.com>
2018-04-23 17:35:03 -0400

    partial DB connection to Patient side

--- HospitalManagement/Patient/views.py
+++ HospitalManagement/Patient/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
+from .HospitalDBConnect import *
 
 def home(request):
     ''' a patient private home page '''
@@ -12,8 +13,10 @@ def home(request):
 
 def appointments(request):
     ''' handles the creation and display of appts '''
+    all_appointments = view_Appointments()
+    context = {'appts': all_appointments}
 
-    return HttpResponse(render(request, 'Patient/appt.html'))
+    return HttpResponse(render(request, 'Patient/appt.html', context))
 
 
 def new_appointments(request):
@@ -21,10 +24,11 @@ def new_appointments(request):
 
     if request.method == 'POST':
         # get the data
-        doctor = request.POST.get("preferedDoctor", "")
-        apt_date = request.POST.get("preferedDate", "")
-        reason = request.POST.get("reason", "")
-
+        PatientID = request.user.id
+        DocID = request.POST.get("preferedDoctor", "")
+        Date = request.POST.get("preferedDate", "")
+        Reason = request.POST.get("reason", "")
+        InsertAppt(PatientID, DocID, Date, Reason)
         #TODO insert into db
         return redirect("/patient/appointments/")
 
@@ -34,6 +38,9 @@ def new_appointments(request):
 
 def medications(request):
     ''' mediations info page'''
+    PatientID = request.user.id
+    all_treatments = view_Treatment(PatientID)
+    context = {'treatments': all_treatments}
 
     # no post, handle all as get
     return HttpResponse(render(request, 'Patient/medications.html'))
@@ -49,7 +56,9 @@ def bills(request):
     ''' shows the user profile '''
 
     # also no posts, all get
-    return HttpResponse(render(request, 'Patient/bills.html'))
+    all_bills = view_Bills()
+    context = {'bills': all_bills}
+    return HttpResponse(render(request, 'Patient/bills.html', context))
 
 def bills_more(request):
     ''' shows the user profile '''
