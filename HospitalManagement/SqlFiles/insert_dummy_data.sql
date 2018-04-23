37ab70f625f91a3dad63ccee9c773faaed19f63c
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-04-22 22:58:11 -0400

    sql statements to create tables and insert data

---
+++ HospitalManagement/SqlFiles/insert_dummy_data.sql
@@ -0,0 +1,141 @@
+use Hospital; 
+
+#Patients
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (101,'Richard','Maggy','1-830-866-2206','P.O. Box 226, 4958 Dictum St.');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (102,'Brock','Mona','1-582-108-2001','270-7263 Dolor. Rd.');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (103,'Jasper','Sophia','1-966-163-1898','P.O. Box 123, 5664 Velit. Road');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (104,'Benedict','Nelle','1-342-927-3983','Ap #801-9140 Id, Road');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (105,'Charles','Maia','1-175-577-9865','382-872 Malesuada Rd.');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (106,'Seth','Kathleen','1-862-540-1806','Ap #682-5855 Interdum Rd.');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (107,'Len','Nina','1-444-923-4868','6985 Lorem Avenue');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (108,'Carl','Wynter','1-306-652-6915','P.O. Box 419, 4376 Adipiscing Avenue');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (109,'Ezekiel','MacKenzie','1-575-995-9953','Ap #397-170 Per Rd.');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (110,'Raymond','Briar','1-100-542-4630','P.O. Box 519, 7954 Natoque Road');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (111,'Damian','Iona','1-228-570-1590','9421 Donec Avenue');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (112,'Tarik','Genevieve','1-520-932-6278','812-9304 Phasellus Avenue');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (113,'Mark','Megan','1-316-386-7515','P.O. Box 584, 3744 Accumsan St.');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (114,'Zeph','Hilary','1-128-300-9734','549 Ipsum Road');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (116,'Lev','Jamalia','1-172-204-3410','8223 Luctus, St.');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (115,'Harrison','Jessamine','1-416-955-4398','Ap #485-3062 Fringilla Av.');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (117,'Noble','Rylee','1-111-166-8378','P.O. Box 970, 2398 Arcu. Rd.');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (118,'Baxter','Cynthia','1-338-463-5317','255-7595 Aliquet Ave');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (119,'Roth','Maile','1-291-511-5804','149-8058 Tellus. Street');
+INSERT INTO patient(PatientID,FirstName,LastName,Phone,Address) VALUES (120,'Uriah','Judith','1-458-627-3349','756-2326 Sed Road');
+
+#Doctor:
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('2', 'Bryar', 'Gil', 'P.O. Box 520, 6416 Montes, Rd.', '4349073182', 'enim@semutdolor.org', '121686');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('3', 'Cody', 'Phyllis', 'P.O. Box 947, 1169 Mollis. Av.', '4340150716', 'Morbi@disparturientmontes.ca', '112271');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('4', 'Brooke', 'Arden', '1084 Lectus. Rd.', '4348026963', 'dolor.Donec.fringilla@orciquis.co.uk', '138368');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('5', 'Zenaida', 'Gloria', 'Ap #146-7737 Rhoncus. Avenue', '4341543710', 'convallis.ante.lectus@faucibusorciluctus.ca', '139619');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('6', 'Nita', 'Kadeem', '442-4766 Molestie. Avenue', '4340929118', 'eget.metus@non.co.uk', '110447');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('7', 'Neville', 'George', 'Ap #813-9001 Pharetra. St.', '4340703550', 'facilisis.lorem.tristique@arcuVestibulum.edu', '111534');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('8', 'Lewis', 'Hedley', 'P.O. Box 762, 2542 Suspendisse Street', '4345644988', 'In@hymenaeosMauris.ca', '112028');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('9', 'Prescott', 'Whitney', '228-7882 Ante Rd.', '4342335219', 'vestibulum.lorem@molestiesodales.ca', '107739');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('10', 'Jennifer', 'Hashim', '2174 Proin St.', '4348972832', 'sed@convallis.co.uk', '105440');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('11', 'Anjolie', 'Linda', '555-412 Ullamcorper. Road', '4347484635', 'egestas.Sed.pharetra@Vivamus.co.uk', '148358');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('12', 'Joseph', 'Cailin', '1861 Ridiculus St.', '4347448813', 'nisi@augueSed.net', '117034');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('13', 'Lysandra', 'Brendan', 'Ap #277-7540 Non Av.', '4341295361', 'dictum@mollis.ca', '138567');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('14', 'Jackson', 'Stewart', 'P.O. Box 413, 9246 Posuere Rd.', '4345727918', 'enim@non.ca', '140749');
+INSERT INTO `hospital`.`Doctor` (`DocID`, `FirstName`, `LastName`, `Address`, `Phone`, `Email`, `Salary`) VALUES ('15', 'Cadman', 'Tatyana', 'Ap #138-7168 A Avenue', '4347066451', 'tempor@velitPellentesque.edu', '116641');
+
+#Nurses:
+INSERT INTO `hospital`.`Nurses` (`NurseID`, `FirstName`, `LastName`, `Phone`, `Address`, `Speciality`, `Email`, `Salary`) VALUES ('1', 'Yardley', 'Ross', '4341337934', '9011 Nulla. St.', 'sp1', 'porta.elit.a@semperduilectus.com', '88290');
+INSERT INTO `hospital`.`Nurses` (`NurseID`, `FirstName`, `LastName`, `Phone`, `Address`, `Speciality`, `Email`, `Salary`) VALUES ('2', 'Ava', 'Winifred', '4349248826', '697-8836 Vestibulum Av.', 'sp2', 'Donec.vitae.erat@dolor.com', '81352');
+INSERT INTO `hospital`.`Nurses` (`NurseID`, `FirstName`, `LastName`, `Phone`, `Address`, `Speciality`, `Email`, `Salary`) VALUES ('3', 'Xander', 'Vielka', '4342434917', 'P.O. Box 117, 6663 Maecenas Ave', 'sp3', 'nibh.Donec.est@nonmagnaNam.net', '86956');
+INSERT INTO `hospital`.`Nurses` (`NurseID`, `FirstName`, `LastName`, `Phone`, `Address`, `Speciality`, `Email`, `Salary`) VALUES ('4', 'Ezekiel', 'Flynn', '4345043670', '201-129 Tincidunt St.', 'sp4', 'eu.enim@cursusnonegestas.edu', '74408');
+INSERT INTO `hospital`.`Nurses` (`NurseID`, `FirstName`, `LastName`, `Phone`, `Address`, `Speciality`, `Email`, `Salary`) VALUES ('5', 'Harding', 'Ursa', '4342785368', 'P.O. Box 461, 7119 Pede Av.', 'sp5', 'hymenaeos@netusetmalesuada.ca', '77996');
+INSERT INTO `hospital`.`Nurses` (`NurseID`, `FirstName`, `LastName`, `Phone`, `Address`, `Speciality`, `Email`, `Salary`) VALUES ('6', 'Bradley', 'Troy', '4341912957', 'P.O. Box 898, 9105 Lorem St.', 'sp6', 'dictum@aauctor.org', '79127');
+INSERT INTO `hospital`.`Nurses` (`NurseID`, `FirstName`, `LastName`, `Phone`, `Address`, `Speciality`, `Email`, `Salary`) VALUES ('7', 'Susan', 'Jana', '4344206419', 'Ap #122-485 Quam. Street', 'sp7', 'tristique@Cumsociisnatoque.co.uk', '89923');
+INSERT INTO `hospital`.`Nurses` (`NurseID`, `FirstName`, `LastName`, `Phone`, `Address`, `Speciality`, `Email`, `Salary`) VALUES ('8', 'Brian', 'Lucas', '4340618866', '192-2111 A Road', 'sp8', 'sed.est@idnuncinterdum.co.uk', '87832');
+INSERT INTO `hospital`.`Nurses` (`NurseID`, `FirstName`, `LastName`, `Phone`, `Address`, `Speciality`, `Email`, `Salary`) VALUES ('9', 'Neville', 'Roth', '4342186859', 'P.O. Box 672, 6954 Posuere, St.', 'sp9', 'Mauris@aduiCras.com', '70194');
+INSERT INTO `hospital`.`Nurses` (`NurseID`, `FirstName`, `LastName`, `Phone`, `Address`, `Speciality`, `Email`, `Salary`) VALUES ('10', 'May', 'Baxter', '4344046500', 'Ap #110-2634 Nulla Rd.', 'sp10', 'nec.metus@imperdietullamcorper.com', '76354');
+
+#Departments:
+INSERT INTO `hospital`.`Department` (`DID`, `DeptName`, `DeptHead`, `DeptDesc`) VALUES ('1', 'Cardiology', 'Ruth Ball', 'Provides medical care to patients who have problems with their heart or circulation');
+INSERT INTO `hospital`.`Department` (`DID`, `DeptName`, `DeptHead`, `DeptDesc`) VALUES ('2', 'Critical Care', 'Lars Gilmore', 'Also called intensive care, this department is for seriously ill patients');
+INSERT INTO `hospital`.`Department` (`DID`, `DeptName`, `DeptHead`, `DeptDesc`) VALUES ('3', 'Diagnostic Imaging', 'Reagan Rutledge', 'Also known as X-Ray Department and/or Radiology Department');
+INSERT INTO `hospital`.`Department` (`DID`, `DeptName`, `DeptHead`, `DeptDesc`) VALUES ('4', 'emergency', 'Donovan Stein', 'Also called Casualty Department, where you\'re likely to be taken if you have arrived in an ambulance or emergency situation');
+INSERT INTO `hospital`.`Department` (`DID`, `DeptName`, `DeptHead`, `DeptDesc`) VALUES ('5', 'General Surgery', 'Alexander Wilson', 'Covers a wide range of types of surgery and procedures on patients');
+
+#Rooms:
+INSERT INTO `hospital`.`Rooms` (`PatientID`, `DepartmentID`,`RoomNumber`, `Capacity`) VALUES ('104','1','101', '1');
+INSERT INTO `hospital`.`Rooms` (`PatientID`, `DepartmentID`,`RoomNumber`, `Capacity`) VALUES ('105','2','102', '1');
+INSERT INTO `hospital`.`Rooms` (`PatientID`, `DepartmentID`,`RoomNumber`, `Capacity`) VALUES ('106','2','104', '2');
+INSERT INTO `hospital`.`Rooms` (`PatientID`, `DepartmentID`,`RoomNumber`, `Capacity`) VALUES ('107','2','105', '2');
+INSERT INTO `hospital`.`Rooms` (`PatientID`, `DepartmentID`,`RoomNumber`, `Capacity`) VALUES ('111','3','201', '4');
+INSERT INTO `hospital`.`Rooms` (`PatientID`, `DepartmentID`,`RoomNumber`, `Capacity`) VALUES ('112','4','301', '8');
+INSERT INTO `hospital`.`Rooms` (`PatientID`, `DepartmentID`,`RoomNumber`, `Capacity`) VALUES ('113','5','302', '8');
+
+#Employees:
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('2', 'Lila', 'Leroy', 'Ap #786-5103 Commodo Av.', '4343295217', 'Food delivery', '2001-01-03', 'Donec.luctus.aliquet@luctuset.net', '40650');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('3', 'Emmanuel', 'Aline', '179-6442 Nisi St.', '4344967091', 'Food delivery', '2010-01-04', 'adipiscing.elit.Etiam@sed.com', '31100');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('4', 'Malik', 'Desiree', 'Ap #366-704 Tempus Avenue', '4346498044', 'Food delivery', '2005-01-05', 'Sed.molestie.Sed@felisullamcorper.org', '36114');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('5', 'Cheyenne', 'Tara', 'P.O. Box 920, 7379 Ullamcorper. Ave', '4349759754', 'Housekeeping', '2014-01-06', 'consectetuer@placeratCrasdictum.edu', '45579');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('6', 'Wilma', 'Clark', 'P.O. Box 627, 8883 A, Avenue', '4344723848', 'Housekeeping', '2011-01-07', 'Cum.sociis.natoque@inconsequatenim.com', '25991');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('7', 'Margaret', 'Silas', 'Ap #576-8423 Nunc. Ave', '4346494382', 'Housekeeping', '1999-01-08', 'et@lobortisquispede.ca', '35666');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('8', 'Ulla', 'Ifeoma', '6419 Eget Street', '4345323396', 'Housekeeping', '2015-01-09', 'Nam.nulla.magna@at.ca', '40609');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('9', 'Kasimir', 'MacKensie', 'Ap #944-3755 Aliquet, Rd.', '4347737398', 'Housekeeping', '2000-01-10', 'lorem@mieleifend.co.uk', '45401');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('10', 'Odysseus', 'Malcolm', '364-7485 Elementum, Rd.', '4349687690', 'Housekeeping', '2013-01-11', 'a@semperet.com', '38161');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('11', 'Silas', 'Karen', 'P.O. Box 308, 360 In Rd.', '4349046005', 'Housekeeping', '2008-01-12', 'consectetuer@magnamalesuadavel.net', '49821');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('12', 'Fitzgerald', 'Geoffrey', 'P.O. Box 321, 8713 Cubilia Street', '4345038730', 'Housekeeping', '1900-01-07', 'nisi.Cum.sociis@molestietellusAenean.edu', '30319');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('13', 'Knox', 'Fuller', '6452 Aliquet. Av.', '4342575438', 'maintenance', '2006-01-14', 'vitae@lectus.com', '28719');
+INSERT INTO `hospital`.`Employees` (`EmployeeID`, `FirstName`, `LastName`, `Address`, `Phone`, `Responsibilities`, `EmploymentDate`, `Email`, `Salary`) VALUES ('14', 'Petra', 'Palmer', 'Ap #640-9396 Gravida. Road', '4341085777', 'maintenance', '2005-01-15', 'blandit.Nam@auctorMauris.co.uk', '47994');
+
+#Bill
+INSERT INTO Bill(PatientID,BillNumber,ReleaseDate,Amount,Description,DueDate) VALUES (101,123,'Jan 27, 2017',500.00,'This is bill 1', 'Jan 27, 2018');
+INSERT INTO Bill(PatientID,BillNumber,ReleaseDate,Amount,Description,DueDate) VALUES (102,144,'Jan 21, 2016',100.00,'This is bill 2', 'Jan 27, 2017');
+INSERT INTO Bill(PatientID,BillNumber,ReleaseDate,Amount,Description,DueDate) VALUES (103,168,'Jan 26, 2018',1500.00,'This is bill 3', 'Jan 27, 2019');
+
+
+#Treatments
+INSERT INTO Treatments(TreatmentID,Ailment,PrescriptionDate,ExpectedOutcome,Warning) VALUES (11,'Allergies',NULL,'This is the expected outcome for Allergies traetment ','This is warning for treatment 11');
+INSERT INTO Treatments(TreatmentID,Ailment,PrescriptionDate,ExpectedOutcome,Warning) VALUES (12,'Diabetes',NULL,'This is the expected outcome for Diabetes traetment ','This is warning for treatment 12');
+INSERT INTO Treatments(TreatmentID,Ailment,PrescriptionDate,ExpectedOutcome,Warning) VALUES (13,'High Blood Pressure',NULL,'This is the expected outcome for High blood pressure traetment ','This is warning for treatment 13');
+
+
+#serves
+INSERT INTO serves(RoomNumber,EmployeeID) VALUES (101,2);
+INSERT INTO serves(RoomNumber,EmployeeID) VALUES (102,4);
+INSERT INTO serves(RoomNumber,EmployeeID) VALUES (104,6);
+INSERT INTO serves(RoomNumber,EmployeeID) VALUES (105,8);
+INSERT INTO serves(RoomNumber,EmployeeID) VALUES (201,9);
+INSERT INTO serves(RoomNumber,EmployeeID) VALUES (301,10);
+
+#admitted_to
+INSERT INTO admitted_to(PatientID,DID) VALUES (101,1);
+INSERT INTO admitted_to(PatientID,DID) VALUES (102,1);
+INSERT INTO admitted_to(PatientID,DID) VALUES (103,2);
+INSERT INTO admitted_to(PatientID,DID) VALUES (104,2);
+INSERT INTO admitted_to(PatientID,DID) VALUES (105,3);
+INSERT INTO admitted_to(PatientID,DID) VALUES (106,3);
+
+
+
+#supports
+INSERT INTO supports(PatientID,NurseID) VALUES (101,3);
+INSERT INTO supports(PatientID,NurseID) VALUES (102,5);
+INSERT INTO supports(PatientID,NurseID) VALUES (103,7);
+INSERT INTO supports(PatientID,NurseID) VALUES (104,6);
+INSERT INTO supports(PatientID,NurseID) VALUES (105,4);
+INSERT INTO supports(PatientID,NurseID) VALUES (106,2);
+INSERT INTO supports(PatientID,NurseID) VALUES (107,8);
+
+
+#treat
+INSERT INTO treat(PatientID,DocID,TreatmentID) VALUES (101,4,12);
+INSERT INTO treat(PatientID,DocID,TreatmentID) VALUES (102,5,12);
+INSERT INTO treat(PatientID,DocID,TreatmentID) VALUES (103,5,12);
+INSERT INTO treat(PatientID,DocID,TreatmentID) VALUES (104,5,11);
+INSERT INTO treat(PatientID,DocID,TreatmentID) VALUES (106,6,11);
+INSERT INTO treat(PatientID,DocID,TreatmentID) VALUES (107,7,13);
+INSERT INTO treat(PatientID,DocID,TreatmentID) VALUES (110,8,13);
+INSERT INTO treat(PatientID,DocID,TreatmentID) VALUES (112,9,13);
+
+
+#Appointment
+INSERT INTO Appointment(PatientID,DocID,ApptDate,Reason) VALUES (101,5,'Apr 1, 2018','this is the reason for patient 101 to make the appointment with Dr 5');
+INSERT INTO Appointment(PatientID,DocID,ApptDate,Reason) VALUES (106,5,'Apr 10, 2018','this is the reason for patient 106 to make the appointment ');
+INSERT INTO Appointment(PatientID,DocID,ApptDate,Reason) VALUES (107,4,'Feb 13, 2018','this is the reason for patient 107 to make the appointment');
+INSERT INTO Appointment(PatientID,DocID,ApptDate,Reason) VALUES (109,8,'Apr 17, 2018','this is the reason for patient 109 to make the appointment');
+
+
